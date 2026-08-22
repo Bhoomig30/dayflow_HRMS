d9931c52bc682c6617df196a30076f53363eeeb3
Adarshini09-star <aadarshinidh@gmail.com>
2026-08-22 11:12:20 +0530

    Delete lib directory

--- lib/config/documents.ts
+++
@@ -1,8 +0,0 @@
-export const MAX_DOCUMENT_SIZE_BYTES = Number(process.env.DAYFLOW_MAX_DOCUMENT_SIZE_BYTES ?? 5 * 1024 * 1024); // 5MB default
-export const ALLOWED_DOCUMENT_TYPES = [
-  "application/pdf",
-  "image/png",
-  "image/jpeg",
-  "application/msword",
-  "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-];
